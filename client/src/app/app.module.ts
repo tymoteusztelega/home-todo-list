ed437eb65b236a2c93f7d4b89d2dfb06c7fa11db
Tymoteusz <tymoteusz.telega@euvic.pl>
2020-03-16 16:20:53 +0100

    Other changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './shared/modules/material.module';
-import { FormsModule } from '@angular/forms';
-import { AuthenticationGuardService } from './authentication/authentication-guard.service';
 import { AuthenticationService } from './authentication/authentication.service';
-import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationGuardService } from './authentication/authentication-guard.service';
 
 @NgModule({
   declarations: [AppComponent],
--- client/src/app/authentication/authentication.module.ts
+++ client/src/app/authentication/authentication.module.ts
@@ -8,10 +8,9 @@ import { MaterialModule } from '../shared/modules/material.module';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SigninFormComponent } from './signin-form/signin-form.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { DetectCapsLockDirective } from '../shared/directives/DetectCapsLock.directive';
 
 @NgModule({
-  declarations: [LoginFormComponent, SigninFormComponent, DetectCapsLockDirective],
+  declarations: [LoginFormComponent, SigninFormComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
